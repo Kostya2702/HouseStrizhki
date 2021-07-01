3e15f3f23706b5e2d7239044d1741a45534735e8
Kostya2702 <loshkarev.kostya27@gmail.com>
2021-07-01 13:52:51 +0500

    Revert "56нп"
    This reverts commit 36b3c883abe19ace8ebd004a5f3317ad1424acdb.

--- firstsite/firstsite/settings.py
+++ firstsite/firstsite/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
     'mainapp.apps.MainappConfig',
 ]
 
@@ -50,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'firstsite.urls'
@@ -130,14 +128,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "/media/"
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
-
-INTERNAL_IPS = [
-    '127.0.0.1',
-]
-
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
-        'LOCATION': os.path.join(BASE_DIR, 'firstsite_cache'),
-    }
-}
--- firstsite/mainapp/urls.py
+++ firstsite/mainapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-from django.views.decorators.cache import cache_page
 
 urlpatterns = [
-    path('', cache_page(90)(views.index), name='home'),
+    path('', views.index, name='home'),
+    path('about', views.about, name='about'),
+    path('create', views.create, name='create'),
 ]
